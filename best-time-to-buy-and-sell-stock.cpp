acc37bd6d1a25b7f46d23e5a7845453f80d50eb8
get.dipen@gmail.com <get.dipen@gmail.com>
2022-11-03 21:23:51 -0700

    calculated max profit

--- best-time-to-buy-and-sell-stock.cpp
+++ best-time-to-buy-and-sell-stock.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main(int argc, char const *argv[]) {
+int maxProfit(vector<int>& prices);
+
+int main(int argc, char const* argv[]) {
+  vector<int> prices1 = {7, 1, 5, 3, 6, 4};
+  vector<int> prices2 = {7, 6, 4, 3, 1};
+
+  int profit1, profit2;
+
+  profit1 = maxProfit(prices1);
+  profit2 = maxProfit(prices2);
+
+  cout << "Max Profit 1: " << profit1 << endl;
+  cout << "Max Profit 2: " << profit2 << endl;
+
   return 0;
+}
+
+int maxProfit(vector<int>& prices) {
+  int buyPrice;
+  int profit = 0;
+  int maxProfit = 0;
+
+  if (prices.size() < 2)
+    return 0;
+
+  for (int i = 0; i < prices.size(); i++) {
+    buyPrice = prices[i];
+    for (int j = i + 1; j < prices.size(); j++) {
+      profit = prices[j] - prices[i];
+      if (maxProfit < profit)
+        maxProfit = profit;
+    }
+  }
+
+  return maxProfit;
 }
