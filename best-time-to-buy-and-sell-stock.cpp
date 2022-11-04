0a4ed23c0af0ea26ea7284678ab1a88ebd4ce870
get.dipen@gmail.com <get.dipen@gmail.com>
2022-11-03 21:46:14 -0700

    better time complexity

--- best-time-to-buy-and-sell-stock.cpp
+++ best-time-to-buy-and-sell-stock.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits>
 #include <vector>
 
 using namespace std;
@@ -21,21 +22,29 @@ int main(int argc, char const* argv[]) {
 }
 
 int maxProfit(vector<int>& prices) {
-  int buyPrice;
   int profit = 0;
   int maxProfit = 0;
+  int minPrice = INT32_MAX;
 
-  if (prices.size() < 2)
-    return 0;
+  if (prices.size() < 2) return 0;
+
+  // brute force
+  // for (int i = 0; i < prices.size(); i++) {
+  //   for (int j = i + 1; j < prices.size(); j++) {
+  //     profit = prices[j] - prices[i];
+  //     if (maxProfit < profit)
+  //       maxProfit = profit;
+  //   }
+  // }
 
   for (int i = 0; i < prices.size(); i++) {
-    buyPrice = prices[i];
-    for (int j = i + 1; j < prices.size(); j++) {
-      profit = prices[j] - prices[i];
-      if (maxProfit < profit)
+    if (prices[i] < minPrice) {
+      minPrice = prices[i];
+    } else {
+      profit = prices[i] - minPrice;
+      if (profit > maxProfit)
         maxProfit = profit;
     }
   }
-
   return maxProfit;
 }
